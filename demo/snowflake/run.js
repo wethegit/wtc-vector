8ae9e90448271aeec350b80276bdc1ec2121e05d
Liam Egan <liam@wethecollective.com>
2017-02-15 10:46:04 -0800

    Updating snowflake example to output the result of the sinewave as a graph ticker

--- demo/snowflake/run.js
+++ demo/snowflake/run.js
@@ -15,6 +15,11 @@ function ready(fn) {
   }
 }
 
+
+let settings = {
+  speed: 0.01,
+}
+
 ready(function() {
 
 
@@ -29,6 +34,9 @@ ready(function() {
   let factor = 2;
   let gravity = new DrawingVector(0, 1);
   let float = new DrawingVector(0.5, 0.5);
+
+  settings.gravityV = gravity.v;
+  settings.floatV = float.v;
 
   // Initiallising the world
   VectorPlayground.init();
@@ -50,9 +58,24 @@ ready(function() {
   VectorPlayground.addVector(float);
 
   let ctx = VectorPlayground.mainCtx;
+  let ctx2 = VectorPlayground.secondarCtx;
 
   let drawSnowflakes = function() {
-    float.v.rotate(0.005);
+    float.v.rotate(settings.speed);
+
+    let dims = VectorPlayground.dimensions;
+    let waveData = ctx2.getImageData(0,0,dims.width, dims.height);
+    ctx2.clearRect(0,0,dims.width, dims.height0,0,dims.width, dims.height);
+    ctx2.putImageData(waveData, 1, 0);
+    ctx2.fillStyle = '#FFFFFF';
+    let pointV = new Vector(VectorPlayground.offset.width + float.v.length * VectorPlayground.scale, VectorPlayground.offset.height + float.v.y * VectorPlayground.scale);
+    ctx2.fillRect(pointV.x, pointV.y, 1, 1);
+
+    ctx.strokeStyle = '#FFFFFF';
+    let floatV = new Vector(VectorPlayground.offset.width + float.v.x * VectorPlayground.scale, VectorPlayground.offset.height + float.v.y * VectorPlayground.scale);
+    ctx.moveTo(floatV.x, floatV.y+1);
+    ctx.lineTo(pointV.x, pointV.y+1);
+    ctx.stroke();
 
     snowflakes.forEach(function(flake, index) {
       // Apply our forces
@@ -71,6 +94,8 @@ ready(function() {
       if(flake.pos.y > window.innerHeight) {
         snowflakes.splice(index, 1);
       }
+
+
     });
   }
 
@@ -84,4 +109,18 @@ ready(function() {
 
 
   draw();
+
+  // Set up the dat gui
+  let prec = [];
+  var gui = new dat.GUI();
+  var speedControl = gui.add(settings, 'speed');
+  let t_gravV = gui.addFolder('Gravity Vector');
+  prec.push(t_gravV.add(settings.gravityV, 'length').listen());
+  let t_sinV = gui.addFolder('Sinewave Vector');
+  prec.push(t_sinV.add(settings.floatV, 'length').listen());
+  t_sinV.open();
+  prec.forEach(function(control) {
+    control.__precision = 3;
+    control.__impliedStep = 0.01;
+  });
 });
